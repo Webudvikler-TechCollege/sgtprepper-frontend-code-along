16c73c88ea2f612eb7c7d06b73d74437eef86c92
Heinz K <heka@techcollege.dk>
2025-10-21 06:25:43 +0200

    Route controller fix

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-import { HomePage } from './pages/homePage/index.js';
-import { NotFoundPage } from './pages/notfoundpage/index.js';
+import { HomePage } from './controllers/homeController.js';
 import { router } from './router/index.js';
 
 const ROOT = document.getElementById("container");
 
 router({
-  '/': async () => await HomePage(), // /index.htm
-  '*': () => NotFoundPage(),
+  '/': () => HomePage(), // /index.htm
 }, '#app');
