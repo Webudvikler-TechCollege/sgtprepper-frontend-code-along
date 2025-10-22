57243b40862ffbbe2f81dcb9324085c434a79b31
Heinz K <heka@techcollege.dk>
2025-10-22 08:41:31 +0200

    Rebase to tewmplate

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,11 +1,4 @@
-import { Div } from "../views/atoms/index.js"
-import { Footer, Header, Main } from "../views/molecules/index.js"
-
 export const HomePage = () => {
-    const div = Div()
-    const header = Header()
-    const main = Main()
-    const footer = Footer()
-    div.append(header,main,footer)
-    return div
+    console.log('Hej verden');
+    
 }
